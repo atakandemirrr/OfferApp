c53f766e10dcd8dc6aee297db4bb044a6416c737
atakandemirrr <atakan1905.demir@gmail.com>
2024-05-21 13:25:14 +0300

    .

--- obj/Debug/net8.0/OfferApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OfferApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfferApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44959dcb2fe7e23993bab30e3e2b81da263a7c51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf167ff7c9dfa40e58fd4a6d6ac948d76a626011")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfferApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfferApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
